af90b1a91999a3275d941b10cbbbeaae6a58fdf5
shibaKhn <shiba@tonicworldwide.com>
2024-09-27 14:34:05 +0530

    done

--- program1.py
+++ program1.py
@@ -1,10 +1,25 @@
 class Solution(object):
-    def isValid(self, s):
+    def isValid(self, s:str)->bool:
         """
         :type s: str
         :rtype: bool
         """
-        pass
+        while True:
+            if '()' in s:
+                s=s.replace('()','')
+            elif '{}' in s:
+                s=s.replace('{}','')
+            elif '[]' in s:
+                s=s.replace('[]','')
+            else:
+                return not s
+            
+if __name__ =='__main__':
+    s="{[()]}"
+    print(f'Is {s}valid ?:{Solution().isValid(s)}')
+
+    s1="{[(])}"
+    print(f'Is {s1}valid ?:{Solution().isValid(s1)}')
 
 
 
--- program2.py
+++ program2.py
@@ -1,10 +1,19 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+    def romanToInt(self, s:str)-> int:
+        roman={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        sum=0
+        for i in range(len(s)-1,-1,-1):
+            num=roman[s[i]]
+            if 3*num < sum:
+                sum=sum-num
+            else:
+                sum=sum+num
+        return sum
+solution = Solution()
+roman_numeral1="MCMXCIV"
+result=solution.romanToInt(roman_numeral1)
+print(result)
+
 
 
 
